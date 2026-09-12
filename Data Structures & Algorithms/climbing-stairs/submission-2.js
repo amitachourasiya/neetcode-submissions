1a9099a6c8eb82abfcbffbff5f99f0af11071d79
amitachourasiya <48174355+amitachourasiya@users.noreply.github.com>
2026-09-12 17:36:09 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let arr = new Array(n).fill(0);
+        arr[0] = 1;
+        arr[1] = 1;
+       for(let i=2;i<=n;i++){
+            arr[i] = arr[i-1] + arr[i-2];
+       }
+       return arr[n];
+    }
+}
